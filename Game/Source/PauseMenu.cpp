a7463a6ebe2294604b3c861809fd5c6540e61e60
errico6 <125646224+errico6@users.noreply.github.com>
2024-04-10 12:02:13 +0200

    Empiezo a implementar el menu de pausa

---
+++ Game/Source/PauseMenu.cpp
@@ -0,0 +1,17 @@
+#include "PauseMenu.h"
+#include "App.h"
+#include "Render.h"
+#include "Textures.h"
+#include "Audio.h"
+#include "GuiManager.h"
+#include "GuiControl.h"
+#include "GuiControlButton.h"
+#include "SDL/include/SDL.h"
+
+PauseMenu::PauseMenu(bool startEnabled) : Module()
+{
+}
+
+// Destructor
+PauseMenu::~PauseMenu()
+{}
---
+++ Game/Source/PauseMenu.h
@@ -0,0 +1,40 @@
+#pragma once
+#include "Module.h"
+#include "SDL/include/SDL.h"
+#include "Window.h"
+#include "GuiControlButton.h"
+#include "GuiManager.h"
+#include "GuiControl.h"
+#include "Timer.h"
+
+class PauseMenu : public Module
+{
+public:
+
+    PauseMenu(bool startEnabled = true);
+    virtual ~PauseMenu();
+
+    bool Start();
+
+    bool Update(float dt);
+
+    bool PostUpdate();
+
+    bool CleanUp();
+
+    void CreatePauseButtons();
+
+public:
+
+    SDL_Texture* continuar;
+    SDL_Texture* ajustes;
+    SDL_Texture* salir;
+    int titleSound;
+    int menuIndex = 1;
+    
+
+   
+
+    //gui list
+    List<GuiControl*> titleButtons;
+};
