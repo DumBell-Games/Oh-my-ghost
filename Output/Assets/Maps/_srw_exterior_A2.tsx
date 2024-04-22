ebf6172cfa82f776ed34d13e7cffd174a154c7d9
Pau Hernández Vázquez <114944532+PauHeer@users.noreply.github.com>
2024-04-22 11:38:03 +0200

    map exterior

---
+++ Output/Assets/Maps/_srw_exterior_A2.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="_srw_exterior_A2" tilewidth="128" tileheight="128" tilecount="432" columns="24">
+ <image source="exterior-pack-free_version/rpgmaker/MZ/tilesets/_srw_exterior_A2.png" width="3072" height="2304"/>
+</tileset>
---
+++ Output/Assets/Maps/_srw_tileset_0.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.1" name="_srw_tileset_0" tilewidth="128" tileheight="128" tilecount="576" columns="24">
+ <image source="exterior-pack-free_version/rpgmaker/MZ/tilesets/_srw_tileset_0.png" width="3072" height="3072"/>
+</tileset>
