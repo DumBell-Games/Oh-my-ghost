f5fb1be32c00322ca18c677c01a6c484886b5fd2
Pau Hernández Vázquez <114944532+PauHeer@users.noreply.github.com>
2024-04-23 16:36:58 +0200

    Map Working

--- Output/Assets/Maps/map.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.1" name="map" tilewidth="128" tileheight="128" tilecount="4644" columns="86">
- <image source="map.jpg" width="11082" height="6912"/>
-</tileset>
