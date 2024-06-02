f75a4d9a5abc9c218ffe3838530b096bbb73385b
Edgar Mesa <118179227+edgarmd1@users.noreply.github.com>
2024-06-02 17:34:35 +0200

    Otro exito más, el video funciona de puta madre y hasta se puede ver una peli y todo si quieres metodo LPA
    Co-Authored-By: Pau Mena Torres <117993994+PauMenaTorres@users.noreply.github.com>

--- Game/Source/Video.cpp
+++ Game/Source/Video.cpp
@@ -15,7 +15,7 @@ Video::~Video()
 {
 }
 
-bool Video::Awake(pugi::xml_node &)
+bool Video::Awake(pugi::xml_node&)
 {
 	return true;
 }
@@ -33,59 +33,66 @@ void Video::Initialize(char* path)
 
 void Video::OpenAVI(LPCSTR path)
 {
-	AVIFileInit();                          // Opens The AVIFile Library
+	AVIFileInit(); // Inicializa la biblioteca AVIFile
 
-	/*TODO 2.1: Use AVIStreamOpenFromFile(...) to open a single stream from the AVI file.
-				- The first parameter is a pointer to a buffer that receives the stream handle.
-				- The second parameter is the path to the file.
-				- The third parameter is the type of stream we want to open (in this case streamtypeVIDEO).
-				- The fourth parameter is which video stream we want (there can be more than one), in this case: 0.
-	*/
-	if (AVIStreamOpenFromFile(&pavi, path, streamtypeVIDEO, 0, OF_READ, NULL) != 0) // Opens The AVI Stream
+	if (AVIStreamOpenFromFile(&pavi, path, streamtypeVIDEO, 0, OF_READ, NULL) != 0) // Abre el flujo de video AVI
+	{
 		LOG("Failed To Open The AVI Stream");
+		return;
+	}
 
+	AVIStreamInfo(pavi, &psi, sizeof(psi)); // Lee la información del flujo en psi
+	width = psi.rcFrame.right - psi.rcFrame.left; // Ancho del fotograma
+	height = psi.rcFrame.bottom - psi.rcFrame.top; // Altura del fotograma
+	lastFrame = AVIStreamLength(pavi); // Último fotograma del flujo
 
-
-	// Uncomment this when finished TODO 2
-	
-	AVIStreamInfo(pavi, &psi, sizeof(psi));					// Reads Information About The Stream Into psi
-	width = psi.rcFrame.right - psi.rcFrame.left;           // Width Is Right Side Of Frame Minus Left
-	height = psi.rcFrame.bottom - psi.rcFrame.top;          // Height Is Bottom Of Frame Minus Top
-	lastFrame = AVIStreamLength(pavi);						// The Last Frame Of The Stream
-
-	// TODO 3.1: Use AVIStreamFrameOpen(...) to decompress video frames from the AVI file. On the second parameter you can pass AVIGETFRAMEF_BESTDISPLAYFMT to select the best display format.
-	// Hint: this function returns a PGETFRAME
-	pgf = AVIStreamGetFrameOpen(pavi, (LPBITMAPINFOHEADER)AVIGETFRAMEF_BESTDISPLAYFMT);              // Create The PGETFRAME Using Our Request Mode
+	// Intenta abrir el frame del video
+	pgf = AVIStreamGetFrameOpen(pavi, (LPBITMAPINFOHEADER)AVIGETFRAMEF_BESTDISPLAYFMT); // Crear PGETFRAME usando nuestro modo de solicitud
 	if (pgf == NULL)
-		LOG("Failed To Open The AVI Frame");	
+	{
+		LOG("Failed To Open The AVI Frame");
+		return;
+	}
 
 	isVideoFinished = false;
 }
 
 bool Video::GrabAVIFrame()
 {
-	//Uncomment this after you have finished TODO 3.
-	LPBITMAPINFOHEADER lpbi;													 // Holds The Bitmap Header Information
-	lpbi = (LPBITMAPINFOHEADER)AVIStreamGetFrame(pgf, frame);					// Grab Data From The AVI Stream
-	pdata = (char *)lpbi + lpbi->biSize + lpbi->biClrUsed * sizeof(RGBQUAD);    // Pointer To Data Returned By AVIStreamGetFrame
-																				// (Skip The Header Info To Get To The Data)
-																				// Convert Data To Requested Bitmap Format
-	
-	/*TODO 5.1: Create a surface using the bitmap data we have above this TODO, and create the texture of the frame with that surface (use LoadSurface from textures module)
-				- pdata holds the texture data (pixels)
-				- biBitCount holds the depht in bits and is contained in the LPBITMAPINFOHEADER structure
-				- pitch is the length of a row of pixels in bytes (widht x 3)
-	*/	
-	SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pdata, width, height, lpbi->biBitCount, width * 3, 0, 0, 0, 0);
-	SDL_Texture* texture = app->tex->LoadSurface(surface);
+	LPBITMAPINFOHEADER lpbi;
+	lpbi = (LPBITMAPINFOHEADER)AVIStreamGetFrame(pgf, frame); // Obtiene datos del flujo AVI
 
-	//TODO 6.1: Blit the texture of the frame.
+	if (lpbi == NULL) // Verifica si lpbi es nulo
+	{
+		LOG("Failed To Get The AVI Frame");
+		return false;
+	}
+
+	pdata = (char*)lpbi + lpbi->biSize + lpbi->biClrUsed * sizeof(RGBQUAD); // Asigna puntero a los datos devueltos por AVIStreamGetFrame
+	if (pdata == NULL) // Verifica si pdata es nulo
+	{
+		LOG("Failed To Get The AVI Frame Data");
+		return false;
+	}
+
+	SDL_Surface* surface = SDL_CreateRGBSurfaceFrom(pdata, width, height, lpbi->biBitCount, width * 3, 0, 0, 0, 0);
+	if (surface == NULL)
+	{
+		LOG("Failed To Create Surface From Frame Data");
+		return false;
+	}
+
+	SDL_Texture* texture = app->tex->LoadSurface(surface);
+	if (texture == NULL)
+	{
+		LOG("Failed To Create Texture From Surface");
+		SDL_FreeSurface(surface);
+		return false;
+	}
+
 	app->render->DrawTexture(texture, 0, 0, NULL);
 
-
-	//TODO 7: Limit the change of the frame to one out of two times.
-	// Hint: We want to blit a diferent frame only when our counter, i, is an even number.
-	if (i % 2 == 0) 
+	if (i % 2 == 0)
 	{
 		frame++;
 	}
@@ -95,18 +102,15 @@ bool Video::GrabAVIFrame()
 		frame = 0;
 		isVideoFinished = true;
 	}
-	
-	// TODO 5.2: Unload the texture and free the surface after the blit.
+
 	app->tex->UnLoad(texture);
 	SDL_FreeSurface(surface);
 
-
-	// BONUS. Window title with information about our video.
-	char   title[100];                     
-	mpf = AVIStreamSampleToTime(pavi, lastFrame) / lastFrame;        // Calculate Rough Milliseconds Per Frame
+	char title[100];
+	mpf = AVIStreamSampleToTime(pavi, lastFrame) / lastFrame;
 	wsprintf(title, "AVI Player: Width: %d, Height: %d, Frames: %d, Miliseconds per frame: %d", width, height, lastFrame, mpf);
 	app->win->SetTitle(title);
-	
+
 	return true;
 }
 
